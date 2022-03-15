3b765370728ae2e7fe353d1591812288dadd531a
Qotrunnada Oktiriani <78190896+qotrunnadao@users.noreply.github.com>
2022-03-15 19:56:38 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
-function myFunction() {
-	document.getElementById("masuk").innerHTML = alert("Hello World!");
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+		sectionId = current.getAttribute("id");
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector(".nav__menu a[href*=" + sectionId + "]")
+				.classList.add("active-link");
+		} else {
+			document
+				.querySelector(".nav__menu a[href*=" + sectionId + "]")
+				.classList.remove("active-link");
+		}
+	});
 }
-
-var selector = ".nav li";
-
-$(selector).on("click", function () {
-	$(selector).removeClass(".active");
-	$(this).addClass(".active");
-});
+window.addEventListener("scroll", scrollActive);
